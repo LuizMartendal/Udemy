33cbf6bb5281bb26866e901305f7c081fdceb3dd
Luiz Henrique Martendal <luiz.martendal52@gmail.com>
2023-03-13 21:52:44 -0300

    .

--- Leandro Costa/REST API S RESTFul do 0 a AWS com Spring Boot e Docker/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/unittests/mockito/services/PersonServiceTest.java
+++ Leandro Costa/REST API S RESTFul do 0 a AWS com Spring Boot e Docker/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/unittests/mockito/services/PersonServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.erudio.unittests.mockito.services;
 
 import br.com.erudio.dtos.PersonDTO;
+import br.com.erudio.exceptions.RequiredObjectIsNullException;
 import br.com.erudio.models.PersonModel;
 import br.com.erudio.repositories.PersonRepository;
 import br.com.erudio.services.PersonService;
@@ -14,7 +15,10 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.BeanUtils;
+import org.springframework.context.annotation.Bean;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -24,7 +28,6 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
 
-    @Mock
     PersonDTO person;
     @Mock
     PersonRepository personRepository;
@@ -38,25 +41,45 @@ class PersonServiceTest {
         MockitoAnnotations.openMocks(this);
     }
 
-    @Test
-    void findAll() {
-    }
-
-    @Test
-    void findById() {
-        UUID id = UUID.fromString("c6218679-9744-4bf8-a87f-c546b34bcf36");
-        person.setId(id);
+    private void setPerson() {
+        person.setId(UUID.fromString("c6218679-9744-4bf8-a87f-c546b34bcf36"));
         person.setName("Luiz");
         person.setAdress("Rua Pedro Martendal");
         person.setLastName("Martendal");
         person.setGender("Male");
+    }
+
+    @Test
+    void ExceptionTest() {
+
+    }
+
+    @Test
+    void findAll() {
+        List<PersonModel> personModelList = new ArrayList<>();
+        when(personRepository.findAll()).thenReturn(personModelList);
+
+        PersonModel personModel = new PersonModel();
+        when(personRepository.save(personModel)).thenReturn(personModel);
+
+        BeanUtils.copyProperties(person, personModel);
+        service.create(person);
+
+        var result = service.findAll();
+
+        assertNotNull(result);
+    }
+
+    @Test
+    void findById() {
+        this.setPerson();
 
         PersonModel personModel = new PersonModel();
         BeanUtils.copyProperties(person, personModel);
         personModel.setId(person.getId());
-        when(personRepository.findById(id)).thenReturn(Optional.of(personModel));
+        when(personRepository.findById(person.getId())).thenReturn(Optional.of(personModel));
 
-        PersonDTO result = service.findById(id);
+        PersonDTO result = service.findById(person.getId());
         assertNotNull(result);
         assertNotNull(result.getId());
         assertNotNull(result.getLinks());
@@ -64,17 +87,68 @@ class PersonServiceTest {
 
     @Test
     void create() {
+        this.setPerson();
+
+        PersonModel personModel = new PersonModel();
+        BeanUtils.copyProperties(person, personModel);
+        when(personRepository.save(personModel)).thenReturn(personModel);
+
+        PersonDTO result = service.create(person);
+        assertNotNull(result);
+        assertNotNull(result.getId());
+        assertNotNull(result.getLinks());
     }
 
     @Test
     void update() {
+        this.setPerson();
+
+        PersonModel personModel = new PersonModel();
+
+        BeanUtils.copyProperties(person, personModel);
+        when(personRepository.findById(personModel.getId())).thenReturn(Optional.of(personModel));
+
+        PersonDTO personDTO = service.findById(personModel.getId());
+        assertNotNull(personDTO);
+        assertNotNull(personDTO.getPersonId());
+
+        personModel.setName("Samara dos");
+        personModel.setLastName("Anjos");
+        when(personRepository.save(personModel)).thenReturn(personModel);
+
+        PersonDTO personDTO1 = new PersonDTO();
+        BeanUtils.copyProperties(personModel, personDTO1);
+        var result = service.update(personDTO1, personDTO1.getId());
+        var resultFound = service.findById(personDTO1.getId());
+
+        assertNotNull(result);
+        assertNotNull(result.getPersonId());
+
+        assertEquals(result.getName(), resultFound.getName());
+        assertNotEquals(resultFound.getName(), personDTO.getName());
     }
 
     @Test
     void delete() {
+        PersonModel personModel = new PersonModel();
+
+        BeanUtils.copyProperties(person, personModel);
+
+        when(personRepository.save(personModel)).thenReturn(personModel);
+        when(personRepository.findById(personModel.getId())).thenReturn(Optional.of(personModel));
+
+        var result = service.create(person);
+
+        var result1 = service.findById(personModel.getId());
+
+        assertEquals(result1.getName(), result.getName());
+        assertEquals(result.getName(), person.getName());
+
+        service.delete(result1.getId());
+
+        var result2 = service.findById(result1.getId());
+
+        assertNull(result2.getName());
     }
 
-    @Test
-    void createV2() {
-    }
 }
